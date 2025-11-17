afb6007f16971a64097d3119b7e3db341f05921d
Shibaji Debnath <imshibaji@gmail.com>
2025-11-17 11:14:04 +0530

    Test Tools Added

---
+++ test/home.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Home Page', () => {
+  it('should load the home page successfully', () => {
+    expect('CT REALTOR').toBeDefined();
+  });
+});
